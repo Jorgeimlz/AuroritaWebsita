77e6182afb27f20d84a56502b15710e1789eab81
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2024-01-23 11:54:37 -0500

    Agregar archivos de proyecto.

---
+++ AuroritaWebsita/Models/ingresarFiestas.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuroritaWebsita.Models
+{
+
+    public class Rootobject
+    {
+        public string nombreFiesta { get; set; }
+        public string imagen { get; set; }
+    }
+
+
+}
